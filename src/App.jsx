d9a27910d4883f0113514a7eba7cdbe9a8d34b97
AntonyReyes <areyesf20_2@unc.edu.pe>
2022-09-17 11:10:58 -0500

    create home and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import ProtectedRoutes from './ProtectedRoutes'
+import Login from './components/Login'
+import './components/styles/Login.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <header className='header'>
+
+        <nav className='header_nav'>
+          <ul className='header_list'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/login'> login </Link></li>
+          </ul>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+
+        <Route element={<ProtectedRoutes />} >
+          {/* <Route path='/pokedex' element={} /> */}
+          {/* <Route path='/pokedex/:name' element={} /> */}
+        </Route>
+      </Routes>
     </div>
   )
 }
