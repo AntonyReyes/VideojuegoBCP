d10fdf1075dfad58b132633c858fc49ddff25949
AntonyReyes <areyesf20_2@unc.edu.pe>
2022-09-18 22:58:30 -0500

    subiendo home styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './styles/home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1>Grupo de </h1>
+      
+    </div>
   )
 }
 
